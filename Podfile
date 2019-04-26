ede7f9d8f45cba9f2d4cd090de629df626efaffb
Scott Moon <interactord@gmail.com>
2019-04-26 22:24:42 +0900

    코코아 팟 라이브러리 및 tavis-cli swift lint 검증 추가

---
+++ Podfile
@@ -0,0 +1,26 @@
+platform :ios, '12.2'
+
+target 'RemakeAppStore' do
+
+  use_frameworks!
+
+  pod 'SwiftLint'
+  pod 'RxSwift', '~> 4.0'
+  pod 'RxCocoa', '~> 4.0'
+
+
+  target 'RemakeAppStoreTests' do
+    inherit! :search_paths
+
+    pod 'SwiftLint'
+    pod 'RxBlocking', '~> 4.0'
+    pod 'RxTest', '~> 4.0'
+
+  end
+
+  target 'RemakeAppStoreUITests' do
+    inherit! :search_paths
+
+  end
+
+end
--- RemakeAppStore/AppDelegate.swift
+++ RemakeAppStore/AppDelegate.swift
@@ -10,20 +10,21 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-	
-	var window: UIWindow?
-	
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		
-		window = UIWindow(frame: UIScreen.main.bounds)
-		
-		let controller = UIViewController()
-		controller.view.backgroundColor = .red
-		window?.rootViewController = controller
-		
-		window?.makeKeyAndVisible()
-		
-		return true
-	}
-}
 
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+
+    let controller = UIViewController()
+    controller.view.backgroundColor = .red
+
+    window?.rootViewController = controller
+
+    window?.makeKeyAndVisible()
+
+    return true
+  }
+
+}
